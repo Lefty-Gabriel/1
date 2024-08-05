5e5df8f95e14472f1e80b9497ff0dae2f0741536
Lefty-Gabriel <165815409+Lefty-Gabriel@users.noreply.github.com>
2024-08-05 11:26:49 -0500

    supletorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, createComponent } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { CreateComponent } from './create/create.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TrayComponent } from './tray/tray.component';
+import { VideoComponent } from "./video/video.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddComponent, CreateComponent, HomeComponent, ProfileComponent, TrayComponent, VideoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
